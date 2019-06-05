219a1cfea8d2a4b7aaaf5905f76838a1c0fa7dfb
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-04 18:18:00 -0700

    added crystal images, added reset.css, styles.css, html sections and divs

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+ // the player will guess with numbers instead of letters.
+
+// There will be four crystals displayed as buttons on the page.
+
+// The player will be shown a random number at the START of the game.
+
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+
+// ON CLICK FUNCTION 
+
+// Your game will hide THIS amount until the player clicks a crystal.
+
+// USER clicks on one of the crystals the score will update - score counter 
+
+
+// The player wins if their total score matches the random number from the beginning of the game.
+
+// IF ELSE STATEMENT 
+
+// The player loses if their score goes above the random number.
+
+// LOSES 
+// IF ELSE > RANDOM NUMBER 
+
+//  game restarts whenever the player wins or loses.
+
+// restartGame(); 
+// the player should see a new random number 
+// all crystrals will have FOUR new hidden values 
+
+// users scores will reset to O 
+// score counter will reset to 0 
+
+
+
+// number of games won , loses 
+// no refreshing the game 
